11a77e8324d79046493eb2cf3456f899159bbc0a
unknown <tina.rahimian94@gmail.com>
2024-12-23 16:36:39 -0800

    edit routes

---
+++ main/routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughtController");
+
+// /api/thoughts
+router.route("/").get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
